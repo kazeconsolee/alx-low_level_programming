651631b55d418ff7bc80ebe30769cd27bf2e232d
Kazeconsolee <kazeconsolee@gmail.com>
2023-02-27 15:17:50 -0800

    corrected 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,11 +6,11 @@
  *
  * Return: The length of @str.
  */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int  length = 0;
 
-	while (*str++)
+	while (*(s + length) != '\0')
 		length++;
 
 	return (length);
