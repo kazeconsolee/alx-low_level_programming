6e78f327f95cde29fa92bd9d6ef1311da9a962d9
Kazeconsolee <kazeconsolee@gmail.com>
2023-03-11 22:51:12 -0800

    betty style

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-
